cc97db3852fac8cc609de538d09e7f12c31630ac
kaichiro <kcfg23@gmail.com>
2019-03-13 14:12:39 -0300

    Inserindo comentários e delimitando regions.

--- PesquisaInfAdicionaisRetHP/Form1.Designer.cs
+++ PesquisaInfAdicionaisRetHP/Form1.Designer.cs
@@ -28,13 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.ofdMain = new System.Windows.Forms.OpenFileDialog();
+            this.txtFolderFile = new System.Windows.Forms.TextBox();
+            this.btnProcessFile = new System.Windows.Forms.Button();
+            this.txtDados = new System.Windows.Forms.TextBox();
+            this.pbMain = new System.Windows.Forms.ProgressBar();
+            this.lblNrCorrente = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPercCorrente = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "&Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // ofdMain
+            // 
+            this.ofdMain.FileName = "openFileDialog1";
+            // 
+            // txtFolderFile
+            // 
+            this.txtFolderFile.Location = new System.Drawing.Point(93, 14);
+            this.txtFolderFile.Name = "txtFolderFile";
+            this.txtFolderFile.Size = new System.Drawing.Size(428, 20);
+            this.txtFolderFile.TabIndex = 1;
+            // 
+            // btnProcessFile
+            // 
+            this.btnProcessFile.Location = new System.Drawing.Point(527, 12);
+            this.btnProcessFile.Name = "btnProcessFile";
+            this.btnProcessFile.Size = new System.Drawing.Size(75, 23);
+            this.btnProcessFile.TabIndex = 2;
+            this.btnProcessFile.Text = "&Process File";
+            this.btnProcessFile.UseVisualStyleBackColor = true;
+            this.btnProcessFile.Click += new System.EventHandler(this.btnProcessFile_Click);
+            // 
+            // txtDados
+            // 
+            this.txtDados.Location = new System.Drawing.Point(12, 40);
+            this.txtDados.Multiline = true;
+            this.txtDados.Name = "txtDados";
+            this.txtDados.Size = new System.Drawing.Size(590, 183);
+            this.txtDados.TabIndex = 3;
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(12, 256);
+            this.pbMain.Maximum = 0;
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(590, 23);
+            this.pbMain.TabIndex = 4;
+            // 
+            // lblNrCorrente
+            // 
+            this.lblNrCorrente.AutoSize = true;
+            this.lblNrCorrente.Location = new System.Drawing.Point(498, 240);
+            this.lblNrCorrente.Name = "lblNrCorrente";
+            this.lblNrCorrente.Size = new System.Drawing.Size(37, 13);
+            this.lblNrCorrente.TabIndex = 5;
+            this.lblNrCorrente.Text = "00000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(541, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "/";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(559, 240);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(43, 13);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "000000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(559, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "100%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(541, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "/";
+            // 
+            // lblPercCorrente
+            // 
+            this.lblPercCorrente.AutoSize = true;
+            this.lblPercCorrente.Location = new System.Drawing.Point(498, 282);
+            this.lblPercCorrente.Name = "lblPercCorrente";
+            this.lblPercCorrente.Size = new System.Drawing.Size(37, 13);
+            this.lblPercCorrente.TabIndex = 8;
+            this.lblPercCorrente.Text = "00000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(621, 400);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblPercCorrente);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNrCorrente);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.txtDados);
+            this.Controls.Add(this.btnProcessFile);
+            this.Controls.Add(this.txtFolderFile);
+            this.Controls.Add(this.btnLoadFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.OpenFileDialog ofdMain;
+        private System.Windows.Forms.TextBox txtFolderFile;
+        private System.Windows.Forms.Button btnProcessFile;
+        private System.Windows.Forms.TextBox txtDados;
+        private System.Windows.Forms.ProgressBar pbMain;
+        private System.Windows.Forms.Label lblNrCorrente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPercCorrente;
     }
 }
 
--- PesquisaInfAdicionaisRetHP/Form1.cs
+++ PesquisaInfAdicionaisRetHP/Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace PesquisaInfAdicionaisRetHP
 {
     public partial class Form1 : Form
     {
+        // campo global para guardar o diretório e o nome do arquivo XML a ser explorado
+        public String fileXML = "";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            #region configurando componente OpenFolderDialog
+            ofdMain.Filter = "(*.xml)|*.xml";
+            ofdMain.Title = "Arquivo de Retorno Hermes Pardini";
+            ofdMain.Multiselect = false;
+            #endregion
+
+            if (ofdMain.ShowDialog() == DialogResult.OK)
+            {
+                // atribuindo valor para campo global de diretório e nome do arquivo XML
+                fileXML = ofdMain.FileName;
+                // imprimindo conteúdo de campo global a barra de título do form
+                this.Text = fileXML;
+            }
+
+            // atribuindo conteúdo de campo global ao componente textBox
+            txtFolderFile.Text = fileXML;
+        }
+
+        private void btnProcessFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // limpando componente textBox
+                txtDados.Clear();
+
+                // testando se existe um arquivo que corresponde com o campo global
+                if (System.IO.File.Exists(fileXML))
+                {
+                    // variável que receberá uma estrutura genérica a partir de uma consulta LinqXML
+                    var xyz =
+                        from p in XElement.Load(fileXML)
+                        .Elements("SuperExame")
+                        .Elements("Exame")
+                        .Elements("InfAdicional")
+                        select new
+                        {
+                            // estrutura genérica de dados
+                            CodExmApoio = p.Parent.Parent.Element("CodExmApoio").Value,
+                            InfAdicional = p.Attribute("idInfAdicional").Value,
+                            Descricao = p.Element("Descricao")?.Value,
+                            Valor = p.Element("Valor")?.Value,
+                            flag = 0
+                        };
+
+                    #region configurando progressBar
+                    pbMain.Minimum = 0;
+                    pbMain.Maximum = xyz.Count();
+                    #endregion
+
+                    #region configurando labels
+                    int i = xyz.Count();
+                    lblNrCorrente.Text = (0).ToString();
+                    lblTotal.Text = i.ToString();
+                    lblPercCorrente.Text = (0).ToString();
+                    #endregion
+
+                    #region instanciando e atribuindo valor inicial para uma variável local 
+                    string cabecalho = "if exists(select * from tempdb.dbo.sysobjects o where o.name like '#teste%') drop table #teste;\r\n";
+                    cabecalho += "create table #teste (CodExmApoio varchar(32), InfAdicional int, Descricao varchar(2048), Valor varchar(1024));\r\n\r\n";
+                    #endregion
+
+                    // começando a preencher o textBox de resultado
+                    txtDados.AppendText(cabecalho);
+
+                    // iteração para preencher parte do textBox
+                    foreach (var abc in xyz)
+                    {
+                        // preenchendo textBox
+                        txtDados.AppendText(formataLinha(abc.CodExmApoio, abc.InfAdicional, abc.Descricao, abc.Valor));
+                        // incrementando valor do progressBar
+                        pbMain.Value++;
+
+                        #region mudando dinamicamente conteúdo dos labels
+                        lblNrCorrente.Text = pbMain.Value.ToString();
+                        lblPercCorrente.Text = ((pbMain.Value * 100) / i).ToString();
+                        #endregion
+                    }
+                    // populando textBox (rodapé de conteúdo)
+                    txtDados.AppendText("\r\n\r\nselect COUNT(*) Quantidade, Descricao, Valor from #teste group by Descricao, Valor order by COUNT(*) desc;\n");
+                }
+                // caso não for encontrado algum arquivo, dispara esta mensagem
+                else
+                {
+                    MessageBox.Show("Não foi escolhido algum arquivo!");
+                }
+            }
+            // caso algum erro for acionado, dispara esta mensagem
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // método que retorna uma string formada/customizada
+        private string formataLinha(string CodExmApoio, string InfAdicional, string Descricao, string Valor)
+        {
+            return string.Format("insert into #teste select '{0}', {1}, '{2}', '{3}';\n", CodExmApoio, InfAdicional, Descricao, Valor);
+        }
+
     }
 }
